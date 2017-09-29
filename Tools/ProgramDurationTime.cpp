34a7e8cd98566a385bf1879c6d55c93c9d5baf1f
Kirkley Zhang <laouuzkk@gmail.com>
2017-09-29 21:15:55 +0800

    Create ProgramDurationTime.cpp

---
+++ Tools/ProgramDurationTime.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc,char *argv[])
+{
+	clock_t e,s = clock();                //Get start time
+	system("pause");
+	e = clock() - s;                         //Get end time
+	printf("The time is %.3fs\n",(double)e/1000);
+	system("pause");
+	return 0;
+}
