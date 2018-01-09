4cf2a88a7f6fc84a7c5dc72b887ed329753ab2bc
Laouu Zhang <laouuzkk@gmail.com>
2018-01-09 22:38:20 +0800

    Update ProgramDurationTime.cpp

--- Tools/ProgramDurationTime.cpp
+++ Tools/ProgramDurationTime.cpp
@@ -1,13 +1,14 @@
-//Visual Studio 2012 Express is recommended.
+//此程序可用于获取程序运行时间
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <Windows.h>
 
 int main(int argc,char *argv[])
 {
 	clock_t e,s = clock();                //Get start time
-	system("pause");
+	Sleep(1000);
 	e = clock() - s;                         //Get end time
 	printf("The time is %.3fs\n",(double)e/1000);
 	system("pause");
